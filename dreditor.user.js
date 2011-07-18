5c0561dca46616a6374516efb2255394d4b37f31
sun <sun@unleashedmind.com>
2011-07-18 18:33:10 +0200

    Revert "by sun: Removed obsolete duplicate Drupal.attachBehaviors()."
    This reverts commit a39644fadb47822a2edd6ca041809244819309da.

--- dreditor.user.js
+++ dreditor.user.js
@@ -2049,6 +2049,13 @@ Drupal.behaviors.dreditorIssuesFilterFormReset = function (context) {
   });
 };
 
+/**
+ * Initialize Dreditor.
+ */
+jQuery(document).ready(function () {
+  Drupal.attachBehaviors(this);
+});
+
 // Add custom stylesheet.
 var styles = document.createElement("style");
 styles.setAttribute('type', 'text/css');
