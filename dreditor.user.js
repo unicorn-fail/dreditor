46907c430c0cbbf3f0eda314ef09028a1131a4b4
sun <sun@unleashedmind.com>
2011-09-28 17:00:54 +0200

    - #1289542 by sun: Updated inline image button behavior for changed text formats.

--- dreditor.user.js
+++ dreditor.user.js
@@ -1611,11 +1611,6 @@ Drupal.behaviors.dreditorCommitMessage = function (context) {
  * Attach image attachment inline HTML injector to file attachments.
  */
 Drupal.behaviors.dreditorInlineImage = function (context) {
-  // Do nothing if the user does not have access to the "Documentation" input
-  // format.
-  if (!$('#edit-format-5').length) {
-    return;
-  }
   $('#upload-attachments, #comment-upload-attachments', context).once('dreditor-inlineimage', function () {
     $(this).find('div.description').each(function () {
       var url = $(this).text();
@@ -1623,16 +1618,12 @@ Drupal.behaviors.dreditorInlineImage = function (context) {
       if (!url.match(/\.png$|\.jpg$|\.jpeg$|\.gif$/)) {
         return;
       }
-      // Fix bug in comment_upload's preview issue attachment URLs.
-      url = url.replace(/\/files\/(?!issues\/)/, '/files/issues/');
       // Generate inline image button.
       var $button = $('<a class="dreditor-button dreditor-inlineimage" href="javascript:void(0);">Embed</a>').click(function () {
         var desc = $(this).parent().siblings('input').val();
         var image = '<img src="' + url + '" alt="' + desc + '" />';
         // Append image to issue comment textarea (context is AHAH content here).
         $('#edit-body, #edit-comment').val($('#edit-body, #edit-comment').val() + "\n" + image + "\n");
-        // Ensure the "Documentation" input format is enabled.
-        $('#edit-format-5').select();
         return false;
       });
       // Append inline image button to attachment.
