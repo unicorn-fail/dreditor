28a3c0523555250d7652f59f9a85dba66bfa6b6a
sun <sun@unleashedmind.com>
2011-10-14 23:43:23 +0200

    - #1305486 by IceCreamYou, sun: Added toggle to collapse projects on /project/user.

--- dreditor.user.js
+++ dreditor.user.js
@@ -1898,6 +1898,45 @@ Drupal.dreditor.syntaxAutocomplete.prototype.suggestions.comment = function (nee
  */
 
 /**
+ * Attach collapsing behavior to user project tables.
+ */
+Drupal.behaviors.dreditorProjectsCollapse = function (context) {
+  var enabled = Drupal.storage.load('projectscollapse.status');
+
+  // Add link to toggle this feature.
+  $('<a href="#" class="dreditor-application-toggle"></a>')
+    .text(enabled ? 'Always show projects' : 'Collapse projects')
+    .click(function () {
+      Drupal.storage.save('projectscollapse.status', !enabled);
+      // Reload the current page without refresh from server.
+      window.location.href = window.location.href;
+      return false;
+    })
+    .insertBefore('table.projects:first');
+
+  if (!enabled) {
+    return;
+  }
+  // First table does not have a heading.
+  var $heading = $('h2#sandboxes').clone();
+  $heading.html($heading.html().replace('Sandbox p', 'P'))
+    .removeAttr('id')
+    .insertBefore('table.projects:first');
+
+  $('table.projects', context).once('dreditor-projectscollapse', function () {
+    var $table = $(this);
+    $heading = $table.prevAll('h2').eq(0);
+    $heading.css({ cursor: 'pointer' })
+      .bind('click.projectscollapse', function () {
+        // .slideToggle() forgets about table width in d.o's outdated jQuery
+        // version.
+        $table.toggle();
+      })
+      .triggerHandler('click');
+  });
+};
+
+/**
  * Attach issue count to project issue tables and hide fixed/needs more info issues without update marker.
  */
 Drupal.behaviors.dreditorIssueCount = function (context) {
