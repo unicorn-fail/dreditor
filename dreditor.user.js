57eee377f2c11f827141af370112aaada5878e65
daspeter <daspeter@762870.no-reply.drupal.org>
2012-09-07 06:47:16 +0200

    - #1753972 by das-peter, IceCreamYou, sun: Fixed Dreditor user script stopped working on Firefox 14.0.1 (Function.caller prohibited in Content Scope Runner?).

--- dreditor.user.js
+++ dreditor.user.js
@@ -2,8 +2,10 @@
 // @name           Dreditor
 // @namespace      http://drupal.org/project/dreditor
 // @description    Awesomeness for drupal.org.
+// @icon           https://drupal.org/misc/druplicon.png
 // @author         Daniel F. Kudwien (sun)
-// @version        0.1
+// @version        1.0
+// @grant          none
 // @include        http://drupal.org/*
 // @include        https://drupal.org/*
 // @include        http://*.drupal.org/*
@@ -45,39 +47,7 @@
  *   - setTimeout() approach seems useless?
  *   - Code comments.
  */
-// If not already running in the page, inject this script into the page.
-if (typeof __PAGE_SCOPE_RUN__ == 'undefined') {
-  // Define a closure/function in the global scope in order to reference the
-  // function caller (the function that executes the user script itself).
-  (function page_scope_runner() {
-    // Retrieve the source of this user script.
-    var self_src = '(' + page_scope_runner.caller.toString() + ')();';
-
-    // Add the source to a new SCRIPT DOM element; prepend it with the
-    // __PAGE_SCOPE_RUN__ marker.
-    // Intentionally no scope-wrapping here.
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.textContent = "var __PAGE_SCOPE_RUN__ = true;\n" + self_src;
-
-    // Inject the SCRIPT element into the page.
-    var head = document.getElementsByTagName('head')[0];
-    head.appendChild(script);
-  })();
-
-  // End execution. This code path is only reached in a GreaseMonkey/user
-  // script environment. User script environment implementations differ; not all
-  // browsers (e.g., Opera) understand a return statement here, and it would
-  // also prevent inclusion of this script in unit tests. Therefore, the entire
-  // script needs to be wrapped in a condition.
-}
-// Drupal is undefined when drupal.org is down.
-else if (typeof Drupal == 'undefined') {
-}
-// Execute the script as part of the content page.
-else {
-
-// @todo Implement closure to provide jQuery in $.
+var dreditor_loader = function ($) {
 
 /**
  * @defgroup jquery_extensions jQuery extensions
@@ -2373,3 +2343,36 @@ Drupal.dreditor.updateCheck();
 // End of Content Scope Runner.
 }
 
+// If not already running in the page, inject this script into the page.
+if (typeof __PAGE_SCOPE_RUN__ == 'undefined') {
+  // Define a closure/function in the global scope in order to reference the
+  // function caller (the function that executes the user script itself).
+  (function page_scope_runner() {
+    // Retrieve the source of dreditor_loader, inject and run.
+    var self_src = '(' + dreditor_loader.toString() + ')(jQuery);';
+
+    // Add the source to a new SCRIPT DOM element; prepend it with the
+    // __PAGE_SCOPE_RUN__ marker.
+    // Intentionally no scope-wrapping here.
+    var script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.textContent = "var __PAGE_SCOPE_RUN__ = true;\n" + self_src;
+
+    // Inject the SCRIPT element into the page.
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(script);
+  })();
+
+  // End execution. This code path is only reached in a GreaseMonkey/user
+  // script environment. User script environment implementations differ; not all
+  // browsers (e.g., Opera) understand a return statement here, and it would
+  // also prevent inclusion of this script in unit tests. Therefore, the entire
+  // script needs to be wrapped in a condition.
+}
+// Drupal is undefined when drupal.org is down.
+else if (typeof Drupal == 'undefined') {
+}
+// Execute the script as part of the content page.
+else {
+  dreditor_loader(jQuery);
+}
