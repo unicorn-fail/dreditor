64fb1849f942e317fe16f338170b47b506981bbd
Mark Carver <mark.carver@me.com>
2013-08-25 19:55:14 -0500

    Added chrome installation detection

--- dreditor.user.js
+++ dreditor.user.js
@@ -2760,6 +2760,13 @@ Drupal.behaviors.dreditorIssueClone = function (context) {
  * Initialize Dreditor.
  */
 
+// Enable detection of installed chrome extension on dreditor.org.
+if (window.location.href.match('dreditor.org') && chrome !== undefined) {
+  var isInstalledNode = document.createElement('div');
+  isInstalledNode.id = 'dreditor-is-installed';
+  document.body.appendChild(isInstalledNode);
+}
+
 // Load jQuery UI if necessary.
 if (window.jQuery !== undefined && window.jQuery.fn.jquery === '1.2.6' && window.jQuery.ui === undefined) {
   var script_tag = document.createElement('script');
