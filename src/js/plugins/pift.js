5af2544183139bb1ce38ad8245db9ff4335a2d72
Mark Carver <mark.carver@me.com>
2013-12-09 11:09:48 -0600

    Fixed #88 - "Collapse hidden files" causes 3-5 second locking on issues with many patches

--- src/js/plugins/pift.js
+++ src/js/plugins/pift.js
@@ -48,67 +48,32 @@ Drupal.behaviors.dreditorPIFT = {
     });
 
     $context.find('.field-name-field-issue-changes table.nodechanges-file-changes').each(function() {
-      var $new = $(this);
-      var $parent = $new.parent();
-      $parent.once('dreditor-pift', function () {
-        "use strict";
-        var tables = {
-          'new': $new,
-          'hidden': $new.clone(),
-          'deleted': $new.clone()
-        };
-        for (var status in tables) {
-          tables[status].find('th:last').remove();
-          tables[status].find('tbody tr').each(function() {
-            var $row = $(this);
-            // File row.
-            if ($row.is('.pift-file-info')) {
-              if ($row.find('.nodechanges-file-status').text() !== status) {
-                $row.remove();
+      var $table = $(this);
+      $table.once('dreditor-pift', function () {
+        $table.find('th:last').remove();
+        $table.find('tbody tr').each(function() {
+          var $row = $(this);
+          // File row.
+          if ($row.is('.pift-file-info')) {
+            var $file = $row.find('.nodechanges-file-link .file');
+            var $size = $row.find('.nodechanges-file-size');
+            $file.prepend('<span class="size">' + $size.text() + '</span>');
+            $size.remove();
+          }
+          // PIFT row.
+          else if ($row.is('.pift-test-info')) {
+            var $cell = $row.find('td');
+            $row.prev().find('td').addClass($cell.attr('class'));
+            $cell.find('.pift-operations').prependTo($cell).find('a').each(function () {
+              if (this.innerText === 'View') {
+                this.innerText = Drupal.t('View Results');
               }
-              var $file = $row.find('.nodechanges-file-link .file');
-              var $size = $row.find('.nodechanges-file-size');
-              $file.prepend('<span class="size">' + $size.text() + '</span>');
-              $size.remove();
-            }
-            // PIFT row.
-            else if ($row.is('.pift-test-info')) {
-              if (status !== 'new') {
-                $row.remove();
+              else if (this.innerText === 'Retest') {
+                this.innerText = Drupal.t('Re-test');
               }
-              var $cell = $row.find('td');
-              $row.prev().find('td').addClass($cell.attr('class'));
-              $cell.find('.pift-operations').prependTo($cell).find('a').each(function () {
-                if (this.innerText === 'View') {
-                  this.innerText = Drupal.t('View Results');
-                }
-                else if (this.innerText === 'Retest') {
-                  this.innerText = Drupal.t('Re-test');
-                }
-              });
-            }
-          }); // jshint ignore:line
-        }
-        if (!tables.new.find('tbody tr').length) {
-          tables.new.remove();
-        }
-        var $fieldset,
-          hiddenCount = tables.hidden.find('tbody tr').length,
-          deletedCount = tables.deleted.find('tbody tr').length;
-        if (hiddenCount) {
-          tables.hidden.wrap('<fieldset class="collapsible collapsed"><div class="fieldset-wrapper"></div></fieldset>');
-          $fieldset = tables.hidden.parent().parent();
-          $fieldset.prepend('<legend><span class="fieldset-legend">' + Drupal.formatPlural(hiddenCount, '1 file was hidden', '@count files were hidden') + '</span></legend>');
-          $fieldset.appendTo($parent);
-        }
-        if (deletedCount) {
-          tables.deleted.wrap('<fieldset class="collapsible collapsed"><div class="fieldset-wrapper"></div></fieldset>');
-          $fieldset = tables.deleted.parent().parent();
-          $fieldset.prepend('<legend><span class="fieldset-legend">' + Drupal.formatPlural(deletedCount, '1 file was deleted', '@count files were deleted') + '</span></legend>');
-          $fieldset.appendTo($parent);
-        }
-        Drupal.attachBehaviors($parent);
-        $parent.append('<p><a href="#recent-files">Back to recent files</a></p>');
+            });
+          }
+        }); // jshint ignore:line
       });
     });
   }
