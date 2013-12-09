9aed74f5d4a90b532586b5e7d5d14615ab733781
Mark Carver <mark.carver@me.com>
2013-12-09 13:56:08 -0600

    Fixed #81 -Fixed issues are not all hidden

--- src/js/plugins/issue.markasread.js
+++ src/js/plugins/issue.markasread.js
@@ -4,8 +4,19 @@
 Drupal.behaviors.dreditorIssueMarkAsRead = {
   attach: function (context) {
     $('table.project-issue', context).once('dreditor-issuemarkasread', function () {
-      $(this).find('.marker').addClass('clickable').bind('click.dreditor-markasread', function () {
+      var throbber = '<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>';
+      $(throbber).appendTo(this).hide();
+      var $markers = $(this).find('.marker').addClass('clickable');
+      var $markAll = $('<a href="#" class="dreditor-application-toggle">Mark all as read</a>')
+        .click(function (e) {
+          $(this).append(throbber);
+          $markers.trigger('click.dreditor-markasread');
+          e.preventDefault();
+          e.stopPropagation();
+        });
+      $markers.bind('click.dreditor-markasread', function () {
         var $marker = $(this);
+        $marker.append(throbber);
         var $link = $marker.prev('a');
         $.ajax({
           // The actual HTML page output is irrelevant, so denote that by using
@@ -13,10 +24,17 @@ Drupal.behaviors.dreditorIssueMarkAsRead = {
           type: 'HEAD',
           url: $link.attr('href'),
           complete: function () {
+            $markers = $markers.not($marker);
+            if (!$markers.length) {
+              $markAll.remove();
+            }
             $marker.remove();
           }
         });
       });
+      if ($markers.length) {
+        $markAll.prependTo($(this).parent());
+      }
     });
   }
 };
