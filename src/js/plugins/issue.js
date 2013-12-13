6f35e6c79670a78b93c8eb86ab6e84f913590b30
Clemens Tolboom <clemens@build2be.com>
2013-12-13 15:39:07 +0100

    WIP: get issues values

--- src/js/plugins/issue.js
+++ src/js/plugins/issue.js
@@ -1,4 +1,98 @@
-Drupal.dreditor.issue = {};
+Drupal = Drupal || {};
+Drupal.dreditor = Drupal.dreditor || {};
+
+Drupal.dreditor.issue = {
+  /**
+   * List of values available on issue view or edit page.
+   *
+   * @type object
+   */
+  fields : {
+    title : {
+      id : "edit-title",
+      get: "#page-subtitle",
+      type : 'text'
+    },
+    status : {
+      label : 'Status:',
+      get: '#block-project-issue-issue-metadata .field-name-field-issue-status .field-items',
+      id : "edit-sid",
+      type : 'select'
+    },
+    project_title : {
+      label : 'Project:',
+      get : '#block-project-issue-issue-metadata .field-name-field-project .field-items ',
+      id : "edit-project-info-project-title",
+      type : 'text',
+      hasAjax : true,
+      setFocus: "edit-title"
+    },
+    version : {
+      label : 'Version:',
+      get : '#block-project-issue-issue-metadata .field-name-field-issue-version .field-items ',
+      id : "edit-project-info-rid",
+      type : 'select'
+    },
+    component : {
+      label : 'Component:',
+      get : '#block-project-issue-issue-metadata .field-name-field-issue-component .field-items ',
+      id : 'edit-project-info-component',
+      type : 'select'
+    },
+    priority : {
+      label : 'Priority:',
+      get : '#block-project-issue-issue-metadata .field-name-field-issue-priority .field-items ',
+      id : "edit-priority",
+      type : 'select'
+    },
+    category : {
+      label : 'Category:',
+      get : '#block-project-issue-issue-metadata .field-name-field-issue-category .field-items ',
+      id : "edit-category",
+      type : 'select'
+    },
+    assigned : {
+      label : 'Assigned:',
+      get : '#block-project-issue-issue-metadata .field-name-field-issue-assigned .field-items ',
+      id : "edit-project-info-assigned",
+      type : 'select'
+    },
+    tags : {
+      label : 'Tags:',
+      get: '#block-project-issue-issue-metadata .field-name-taxonomy-vocabulary-9 .field-items .field-item',
+      id : "edit-taxonomy-tags-9",
+      type : 'tags'
+    },
+    comment : {
+      id : "edit-comment",
+      type : 'textarea'
+    }
+  },
+  /**
+   * The current issue values are presented at the top of an issue
+   *
+   * The values are themed into a table id=project-issue-summary-table
+   *
+   * Each row has two column
+   * - first having the label listed above in the fields
+   * - next having the current issue status value
+   */
+  getCurrent : function (oldName) {
+    var name = oldName.replace(/^old/, '').toLowerCase();
+    var f = Drupal.dreditor.issue.fields[name];
+    if (typeof f !== 'undefined') {
+      if (typeof f.label !== 'undefined') {
+        var value = [];
+        $(f.get).each(function(){
+          value.push($(this).text());
+        });
+        return value.join(', ');
+      }
+    }
+    return '';
+  },
+};
+
 /**
  * Gets the issue node id.
  */
@@ -23,8 +117,7 @@ Drupal.dreditor.issue.getNewCommentNumber = function() {
  * Gets the issue title.
  */
 Drupal.dreditor.issue.getIssueTitle = function() {
-  var title = $('#page-subtitle').text() || '';
-  return title;
+  return Drupal.dreditor.issue.getCurrent('title');
 };
 
 /**
@@ -80,8 +173,7 @@ Drupal.dreditor.issue.getSelectedComponent = function() {
  */
 Drupal.dreditor.issue.getSelectedVersion = function() {
   // Retrieve version from the comment form selected option label.
-  var version = $(':input[name*="issue_version"] :selected').text();
-  return version;
+  return Drupal.dreditor.issue.getCurrent('version');
 };
 
 /**
