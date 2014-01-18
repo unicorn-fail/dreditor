a19b10b57a884b1ac8fd815e054290f336e56e56
Scott Reeves <cottser@mailworks.org>
2014-01-18 00:26:49 -0500

    Fix indenting in patch diff code

--- src/js/plugins/patch.review.js
+++ src/js/plugins/patch.review.js
@@ -490,48 +490,48 @@ Drupal.dreditor.patchReview.behaviors.setup = function (context, code) {
     }
     // Colorize new code, but skip file diff lines.
     else if (line.match(/^((?!\+\+\+)\+.*)$/)) {
-        // Expose tabs.
-        line = line.replace(/(\t+)/, '<span class="error tab">$1</span>');
-        // Wrap trailing white-space with a SPAN to expose them during patch
-        // review. Also add a hidden end-of-line character that will only appear
-        // in the pasted code.
-        line = line.replace(/^(.*\S)(\s+)$/, '$1<span class="error whitespace">$2</span><span class="hidden">¶</span>');
+      // Expose tabs.
+      line = line.replace(/(\t+)/, '<span class="error tab">$1</span>');
+      // Wrap trailing white-space with a SPAN to expose them during patch
+      // review. Also add a hidden end-of-line character that will only appear
+      // in the pasted code.
+      line = line.replace(/^(.*\S)(\s+)$/, '$1<span class="error whitespace">$2</span><span class="hidden">¶</span>');
 
-        classes.push('new');
-        diffstat.insertions++;
-        syntax = true;
-        if (ln2) {
-          ln1o = false;
-          ln2++;
-        }
+      classes.push('new');
+      diffstat.insertions++;
+      syntax = true;
+      if (ln2) {
+        ln1o = false;
+        ln2++;
       }
-      // Replace line with a space (so ruler shows up).
-      else if (!line.length) {
-          line = '&nbsp;';
-        }
-        // Match git format-patch EOF lines and reset line count.
-        else if (line.match(/^\-\-$/)) {
-            ln1o = false;
-            ln2o = false;
-            ln1 = '';
-            ln2 = '';
-          }
-          // Detect missing newline at end of file.
-          else if (line.match(/.*No newline at end of file.*/i)) {
-              line = '<span class="error eof">' + line + '</span>';
-            }
-            else {
-              // @todo Also colorizing unchanged lines makes added comments almost
-              // invisible. Although we could use .new.comment as CSS selector, the
-              // question of a sane color scheme remains.
-              // syntax = true;
-              if (ln1 && ln1o) {
-                ln1++;
-              }
-              if (ln2 && ln2o) {
-                ln2++;
-              }
-            }
+    }
+    // Replace line with a space (so ruler shows up).
+    else if (!line.length) {
+      line = '&nbsp;';
+    }
+    // Match git format-patch EOF lines and reset line count.
+    else if (line.match(/^\-\-$/)) {
+      ln1o = false;
+      ln2o = false;
+      ln1 = '';
+      ln2 = '';
+    }
+    // Detect missing newline at end of file.
+    else if (line.match(/.*No newline at end of file.*/i)) {
+      line = '<span class="error eof">' + line + '</span>';
+    }
+    else {
+      // @todo Also colorizing unchanged lines makes added comments almost
+      // invisible. Although we could use .new.comment as CSS selector, the
+      // question of a sane color scheme remains.
+      // syntax = true;
+      if (ln1 && ln1o) {
+        ln1++;
+      }
+      if (ln2 && ln2o) {
+        ln2++;
+      }
+    }
     // Colorize comments.
     if (syntax && line.match(/^.\s*\/\/|^.\s*\/\*[\* ]|^.\s+\*/)) {
       classes.push('comment');
