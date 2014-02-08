d1e6327c948261819b357879035b4d64f2fe93df
sun <sun@unleashedmind.com>
2014-02-08 20:41:45 +0100

    Fixed placement/position of image embed buttons.

--- src/js/plugins/inline.image.js
+++ src/js/plugins/inline.image.js
@@ -5,7 +5,8 @@ Drupal.behaviors.dreditorInlineImage = {
   attach: function (context) {
     var $context = $(context);
     var $comment = $(':input[name="nodechanges_comment_body[value]"]');
-    $context.find('.file > a').once('dreditor-inlineimage', function () {
+    var $elements = $context.find('.file').once('dreditor-inlineimage');
+    $elements.find('> a').each(function () {
       var $link = $(this);
       var url = $link.attr('href');
       // Only process image attachments.
@@ -15,7 +16,7 @@ Drupal.behaviors.dreditorInlineImage = {
       // Generate inline image button (cannot be <a>, other scripts bind links).
       var $button = $('<span class="dreditor-button dreditor-inlineimage">Embed</span>');
       // Append inline image button to attachment.
-      $link.parent().parent().append($button);
+      $link.parent().prepend($button);
       // Override click event.
       $button
         .bind('click', function (e) {
