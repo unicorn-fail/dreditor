4b112807f48e080ae0022aa1ede55b49a5408b98
sun <sun@unleashedmind.com>
2014-05-31 17:54:28 +0200

    Fixed comment jump link autocompletion after d.o user pictures deployment.

--- src/js/plugins/syntax.autocomplete.js
+++ src/js/plugins/syntax.autocomplete.js
@@ -245,7 +245,7 @@ Drupal.dreditor.syntaxAutocomplete.prototype.suggestions.comment = function (nee
       };
       // Add issue author to comment authors and build the suggestion list.
       var n, id;
-      $('.comment > a').each(function () {
+      $('.comment a.permalink').each(function () {
         n = this.text.substring(9);
         id = this.hash.substring(1);
         self.suggestionCommentList[n] = id;
