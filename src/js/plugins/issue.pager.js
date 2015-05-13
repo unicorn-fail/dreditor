f1b353d7149049229c565bf71ddc5e2ab1160c6c
Ariel Barreiro <abarrei@gmail.com>
2015-05-13 10:02:01 -0300

    Indentation to spaces

--- src/js/plugins/issue.pager.js
+++ src/js/plugins/issue.pager.js
@@ -2,7 +2,7 @@
  * Duplicate the pager on top, can be handy at times.
  */
 Drupal.behaviors.issuePager = {
-	attach: function (context) {
-		$('.comments', context).prepend($('.item-list').has('.pager').clone());
-	}
+  attach: function (context) {
+    $('.comments', context).prepend($('.item-list').has('.pager').clone());
+  }
 };
